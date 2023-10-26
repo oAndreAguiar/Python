13318a99064d2825137097665d9dc92c3bdf8297
Andre Felipe Aguiar <131310474+oAndreAguiar@users.noreply.github.com>
2023-10-26 11:56:45 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+# 1) Crie um script Python que peça o nome e o número de matrícula de um aluno.
+
+nome = str(input("Digite seu nome:"))
+matricula = int(input("Digite sua matrícula:"))
+print(f"Seu nome é {nome} \nE sua matrícula é {matricula}")
+print( 'o tipo de dado da var "nome"é')
+print(type(nome))
+print( 'o tipo de dado da var "matricula" é')
+print(type(matricula))
